315504b75fae13ce43775d3c09af62de3dc2fc3f
Danilo Silva <contact@danilocgsilva.me>
2026-07-29 13:38:09 -0300

    First commit

---
+++ src/RegisterConnectionCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Danilocgsilva\EntityCloneCli;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Question\Question;
+
+#[AsCommand(
+    name: 'app:register-connection',
+    description: 'Register a database connection credentials.'
+)]
+class RegisterConnectionCommand extends Command
+{
+    private InputInterface $input;
+
+    private OutputInterface $output;
+    
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->input = $input;
+        $this->output = $output;
+        /** @var \Symfony\Component\Console\Helper\QuestionHelper */
+        $helper = $this->getHelper('question');
+
+        $hostQuestion = new Question('Enter the host: ');
+        $hostQuestion->setValidator(function ($value) {
+            if (empty(trim($value))) {
+                throw new \Exception('Host cannot be empty.');
+            }
+            return trim($value);
+        });
+        $host = $helper->ask($input, $output, $hostQuestion);
+
+        $userQuestion = new Question('Enter the user: ');
+        $userQuestion->setValidator(function ($value) {
+            if (empty(trim($value))) {
+                throw new \Exception('User cannot be empty.');
+            }
+            return trim($value);
+        });
+        $user = $helper->ask($input, $output, $userQuestion);
+
+        $dbQuestion = new Question('Enter the database name: ');
+        $dbQuestion->setValidator(function ($value) {
+            if (empty(trim($value))) {
+                throw new \Exception('Database name cannot be empty.');
+            }
+            return trim($value);
+        });
+        $databaseName = $helper->ask($input, $output, $dbQuestion);
+
+        $passwordQuestion = new Question('Enter the password: ');
+        $passwordQuestion->setHidden(true);
+        $passwordQuestion->setHiddenFallback(false); // Fail if terminal doesn't support hiding
+        $passwordQuestion->setValidator(function ($value) {
+            if (empty(trim($value))) {
+                throw new \Exception('Password cannot be empty.');
+            }
+            return trim($value);
+        });
+        $password = $helper->ask($input, $output, $passwordQuestion);
+
+        $portQuestion = new Question('Enter the port [default: 3306]: ', '3306');
+        $port = trim($helper->ask($input, $output, $portQuestion));
+
+        $output->writeln('<info>Connection details collected successfully!</info>');
+        $output->writeln([
+            'Host: <comment>' . $host . '</comment>',
+            'User: <comment>' . $user . '</comment>',
+            'Database: <comment>' . $databaseName . '</comment>',
+            'Port: <comment>' . $port . '</comment>',
+        ]);
+
+        return Command::SUCCESS;
+    }
+}
